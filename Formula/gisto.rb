0a4f2d9637b6c53d847ba12725331cf37a85be06
Sasha Khamkov <sanusart@gmail.com>
2026-04-08 14:47:31 +0300

    Update Gisto to 2.7.0 with correct SHA256

---
+++ Formula/gisto.rb
@@ -0,0 +1,36 @@
+class Gisto < Formula
+  desc "Cross-platform snippets management desktop application"
+  homepage "https://github.com/Gisto/Gisto"
+  version "2.7.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/Gisto/Gisto/releases/download/v2.7.0/Gisto_aarch64.app.tar.gz"
+      sha256 "5503a126654ef3c4559ac90c90326368b653d1b3f05bc7f56dfcca6fb0a1ed73"
+    else
+      url "https://github.com/Gisto/Gisto/releases/download/v2.7.0/Gisto_x64.app.tar.gz"
+      sha256 "6cba1cb21deb1f3a0812e06ac3c106f648d8e2b48ef0dfdce958f11f15ebc187"
+    end
+  end
+
+  def install
+    prefix.install "Gisto.app"
+  end
+
+  def post_install
+    return unless OS.mac?
+
+    appdir = HOMEBREW_PREFIX/"Applications"
+    appdir.mkpath unless appdir.exist?
+    return if (appdir/"Gisto.app").exist?
+
+    cp_r "#{prefix}/Gisto.app", appdir
+  end
+
+  def caveats
+    <<~EOS
+      Gisto is not code-signed. On macOS, you may need to run:
+        xattr -dr com.apple.quarantine /Applications/Gisto.app
+    EOS
+  end
+end
