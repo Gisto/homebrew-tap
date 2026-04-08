28580f1a87188161c239462dfbcd75f806669ddb
Sasha Khamkov <sanusart@gmail.com>
2026-04-08 15:22:31 +0300

    fix: add more debug to diagnose

--- Formula/gisto.rb
+++ Formula/gisto.rb
@@ -14,27 +14,38 @@ class Gisto < Formula
   end
 
   def install
-    # Handle whatever structure we got from the tarball
+    puts "DEBUG: install method starting"
+    puts "DEBUG: buildpath = #{buildpath}"
+    puts "DEBUG: prefix = #{prefix}"
+    system "ls", "-laR", buildpath.to_s
+    
     if (buildpath/"Gisto.app").exist?
+      puts "DEBUG: Found Gisto.app in buildpath"
       FileUtils.mv (buildpath/"Gisto.app"), prefix/"Gisto.app"
     elsif (buildpath/"Contents").exist?
+      puts "DEBUG: Found Contents in buildpath"
       (prefix/"Gisto.app").mkpath
       FileUtils.mv (buildpath/"Contents"), prefix/"Gisto.app/Contents"
     else
-      puts "DEBUG: Contents of buildpath:"
-      system "ls", "-laR", buildpath.to_s
-      raise "Could not find Gisto.app or Contents in build directory"
+      puts "DEBUG: Neither Gisto.app nor Contents found!"
+      raise "Could not find Gisto.app or Contents"
     end
+    puts "DEBUG: install completed"
   end
 
   def post_install
-    return unless OS.mac?
-
+    puts "DEBUG: post_install starting"
     appdir = HOMEBREW_PREFIX/"Applications"
-    appdir.mkpath unless appdir.exist?
-    return if (appdir/"Gisto.app").exist?
-
-    cp_r "#{prefix}/Gisto.app", appdir
+    puts "DEBUG: appdir = #{appdir}"
+    puts "DEBUG: prefix = #{prefix}"
+    puts "DEBUG: Checking for #{prefix}/Gisto.app"
+    if (prefix/"Gisto.app").exist?
+      puts "DEBUG: Gisto.app exists at prefix, copying to Applications"
+      appdir.mkpath unless appdir.exist?
+      cp_r "#{prefix}/Gisto.app", appdir
+    else
+      puts "DEBUG: ERROR - Gisto.app NOT found at prefix!"
+    end
   end
 
   def caveats
