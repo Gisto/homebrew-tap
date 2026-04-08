faf922b97996243ff2c68f1cedddffcefb0d1ab5
Sasha Khamkov <sanusart@gmail.com>
2026-04-08 15:15:40 +0000

    Update Gisto to v2.7.0

--- Casks/gisto.rb
+++ Casks/gisto.rb
@@ -5,10 +5,7 @@ cask "gisto" do
   sha256 arm:   "b97ff9f912b0e4eaa8a2e644d38616d71009dcc091f76ff12cfb7b94e83c4485",
          intel: "c0647e4e7c0bc4d4c72fcec1087137695c3cb05f29033fbe260288bff6b50d97"
 
-  url do
-    base_url = "https://github.com/Gisto/Gisto/releases/download/v2.7.0/Gisto_2.7.0_<arch>.dmg"
-    base_url.gsub("<arch>", Hardware::CPU.arch == :arm ? "aarch64" : "x64")
-  end
+  url "https://github.com/Gisto/Gisto/releases/download/v#{version}/Gisto_#{version}_#{arch}.dmg"
   name "Gisto"
   desc "Cross-platform snippets management desktop application"
   homepage "https://www.gisto.org/"
