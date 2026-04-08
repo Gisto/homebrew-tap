2b270c183d6ea1dc505d31cc5d36c67006fd1075
Sasha Khamkov <sanusart@gmail.com>
2026-04-08 11:32:37 +0000

    Update Gisto to v2.7.0

--- Formula/gisto.rb
+++ Formula/gisto.rb
@@ -1,15 +1,15 @@
 class Gisto < Formula
   desc "Cross-platform snippets management desktop application"
   homepage "https://github.com/Gisto/Gisto"
-  version ""
+  version "2.7.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Gisto/Gisto/releases/download/v/Gisto_aarch64.app.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/Gisto/Gisto/releases/download/v2.7.0/Gisto_aarch64.app.tar.gz"
+      sha256 "964b579d8be7145d0f289a6b7fc4f0de4e3d6b54cafaa5ef43b8c51d9992fe6f"
     else
-      url "https://github.com/Gisto/Gisto/releases/download/v/Gisto_x64.app.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/Gisto/Gisto/releases/download/v2.7.0/Gisto_x64.app.tar.gz"
+      sha256 "888db67dbdc8a39fe7cf34e75a7c62670ff86c098df7d0102cdb9f28d74ab7a7"
     end
   end
 
